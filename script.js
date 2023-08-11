f6b77039d51d6882e44cfd2c6999d93e363b9552
Shridhar Savant <93203347+shri1918@users.noreply.github.com>
2023-08-11 20:31:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// your code here
+function updateUrl() {
+    var name = document.getElementById("name").value;
+    var year = document.getElementById("year").value;
+    
+    var queryString = "";
+    
+    if (name) {
+        queryString += "?name=" + encodeURIComponent(name);
+    }
+    
+    if (year) {
+        if (queryString === "") {
+            queryString += "?year=" + year;
+        } else {
+            queryString += "&year=" + year;
+        }
+    }
+    
+    document.getElementById("url").textContent = "https://localhost:8080/" + queryString;
+}
